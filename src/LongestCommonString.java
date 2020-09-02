e2738cc097f8754c13102bc26dcf4e819775efd4
Georg Schweinschwaller <georg.schweinschwaller@live.at>
2020-09-02 08:29:05 +0200

    Performance

--- src/LongestCommonString.java
+++ src/LongestCommonString.java
@@ -10,8 +10,8 @@ public class LongestCommonString {
         String strTwo = s.nextLine();
 
         //TEST
-        //String strOne = "AGTCCGTCAAAGT";
-        //String strTwo = "AGTGACCCGTCAACC";
+//Test        String strOne = "AGTCCGTCAAAGT";
+//Test        String strTwo = "AGTGACCCGTCAACC";
 
         if (strOne.length() < strTwo.length()) {
             System.out.println(findLCS(strTwo.toUpperCase(), strOne.toUpperCase()));
@@ -29,32 +29,38 @@ public class LongestCommonString {
         String result = " ";
         String help = "";
 
-        /*
+//Test        int testCount = 0;
         int jmp = 0;
         for (int i = 0; i < strOneLonger.length(); i++) {
+//Test            testCount++;
             if (!strTwo.contains(help)) {
                 if (help.length() > result.length()) {
                     result = help.substring(0, help.length() - 1);
-                    help = "";
                     i = ++jmp;
                 }
+                help = "";
             }
             help += strOneLonger.charAt(i);
         }
-        */
-
+        /*
         for (int i = 0; i < strOneLonger.length(); i++) {
             help = "";
             for (int j = i; j < strOneLonger.length(); j++) {
+                testCount++;
                 help += strOneLonger.charAt(j);
                 if (!strTwo.contains(help)) {
                     if (help.length() - 1 > result.length()) {
                         result = help.substring(0, help.length() - 1);
+                        break;
                     }
                     help = "";
                 }
             }
-        }
+            if (strOneLonger.length() - i < result.length()) {
+                break;
+            }
+        }*/
+//Test        System.out.println("\nSchleifendurchläufe: " + testCount + "\n");
 
         if (result.length() > 2) {
             return result;
